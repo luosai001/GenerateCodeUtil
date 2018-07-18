967f916f6e76835b4f479d5e40833d6920d3965a
luosai <610907778@qq.com>
2018-07-18 20:32:05 +0800

    '代码自动生成'

---
+++ src/main/java/com/code/generate/UtilApplication.java
@@ -0,0 +1,12 @@
+package com.code.generate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtilApplication.class, args);
+	}
+}
---
+++ src/test/java/com/code/generate/UtilApplicationTests.java
@@ -0,0 +1,36 @@
+package com.code.generate;
+
+import com.code.generate.service.SysGeneratorService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UtilApplicationTests {
+
+	@Autowired
+	private SysGeneratorService sysGeneratorService;
+
+	@Test
+	public void generateTables()throws IOException {
+		List<Map<String, Object>> maps = sysGeneratorService.queryList(new HashMap<>());
+		List<String> tableName = maps.stream().map(e -> (String)e.get("tableName")).collect(Collectors.toList());
+		byte[] bytes = sysGeneratorService.generatorCode(tableName.toArray(new String[]{}));
+		File file = new File("E:/talebase.zip");
+		FileOutputStream fileOutputStream = new FileOutputStream(file);
+		fileOutputStream.write(bytes);
+		fileOutputStream.flush();
+		fileOutputStream.close();
+	}
+}
